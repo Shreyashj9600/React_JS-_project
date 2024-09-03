a69685690c4a6a0e9e1823ff138aab1a31eaa651
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-09-03 23:59:12 +0530

    youtubeclone

---
+++ project-12/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className='text-5xl'>
+      file is working test file 
+    </h1>
+  );
+}
+
+export default App;
